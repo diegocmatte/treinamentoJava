9e54d4eb36791b5bfdf93d4f74328c0538670d81
Diego Matté <diegocmatte@gmail.com>
2021-08-18 12:41:19 -0300

    commit

--- src/com/company/lambdas/Foreach.java
+++ src/com/company/lambdas/Foreach.java
@@ -18,5 +18,19 @@ public class Foreach {
         aprovados.forEach(nome ->
             System.out.println(nome + "!!"));
 
+        System.out.println("\nMethod reference 1");
+        aprovados.forEach(System.out::println);
+
+        System.out.println("\nLambda #02..");
+        aprovados.forEach(nome -> meuImprimir(nome));
+
+        System.out.println("\nMethod reference 2");
+        aprovados.forEach(Foreach::meuImprimir);
+
+
+    }
+
+    static void meuImprimir(String nome){
+        System.out.println("Oi, meu nome é "+nome);
     }
 }
---
+++ src/com/company/lambdas/PredicadoComposicao.java
@@ -0,0 +1,21 @@
+package com.company.lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+
+    public static void main(String[] args) {
+
+        Predicate<Integer> isPar = numero -> numero % 2 == 0;
+        Predicate<Integer> isTresDigitos =
+                numero -> numero >= 100 && numero <= 999;
+
+        System.out.println(isPar.test(22));
+        System.out.println(isPar.test(23));
+
+        System.out.println(isPar.and(isTresDigitos).test(122));
+        System.out.println(isPar.and(isTresDigitos).negate().test(123));
+        System.out.println(isPar.or(isTresDigitos).test(123));
+
+    }
+}
