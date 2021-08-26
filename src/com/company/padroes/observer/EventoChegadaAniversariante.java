3c0b92355c39645225bdc22f2d7d2e49ae46446b
Diego Matté <diegocmatte@gmail.com>
2021-08-26 16:49:33 -0300

    padrao observer

---
+++ src/com/company/padroes/observer/EventoChegadaAniversariante.java
@@ -0,0 +1,18 @@
+package com.company.padroes.observer;
+
+import java.util.Date;
+
+public class EventoChegadaAniversariante {
+
+    private final Date momento;
+
+    public EventoChegadaAniversariante(Date momento) {
+        this.momento = momento;
+    }
+
+    public Date getMomento() {
+        return momento;
+    }
+
+    
+}
---
+++ src/com/company/padroes/observer/ObservadorChegadaAniversariante.java
@@ -0,0 +1,7 @@
+package com.company.padroes.observer;
+
+@FunctionalInterface
+public interface ObservadorChegadaAniversariante {
+
+    public void chegou(EventoChegadaAniversariante evento);
+}
